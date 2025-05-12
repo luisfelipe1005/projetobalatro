e2abc6662a3f9b96034e416bd5713699486290ae
luisfelipe1005 <47942553+luisfelipe1005@users.noreply.github.com>
2025-05-12 12:00:37 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,23 +3,27 @@
 
 int main()
 {
-    
-    char baralho[]={ 'A', '2','3','5','5'} ;
+    char baralho[]={ 'AO', 'AE', 'AC', 'AP', '2O', '2E', '2C', '2P','3O', '3E', '3C', '3P','4O', '4E', '4C', '4P','5O', '5E', '5C', '5P','6O', '6E', '6C', '6P','7O', '7E', '7C', '7P','8O', '8E', '8C', '8P','9O', '9E', '9C', '9P','VO', 'VE', 'VC', 'VP','DO', 'DE', 'DC', 'DP','RO', 'RE', 'RC', 'RP',} ;
+
+    int size = sizeof(baralho) / sizeof(baralho[0]);
+    shuffle(baralho,size);
     printf("sua mão é:\n");
-          
-for(i=0;i<=)
-          
-        
+
+for(i=0;i<=5;i++){
+    baralho[x]
+}
+
+
     return 0;
 }
 
 
-void shuffle(int *array, size_t n)
+void shuffle(char *array, size_t n)
 {
-    if (n > 1) 
+    if (n > 1)
     {
         size_t i;
-        for (i = 0; i < n - 1; i++) 
+        for (i = 0; i < n - 1; i++)
         {
           size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
           int t = array[j];

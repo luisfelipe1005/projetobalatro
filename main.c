f2d436b65e6ac8fa08f6b5e757e11f177460987c
luisfelipe1005 <47942553+luisfelipe1005@users.noreply.github.com>
2025-05-12 11:46:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,28 @@
 
 int main()
 {
-    char mao[5]={ 'A', '2','3','5','5'} ;
+    
+    char baralho[]={ 'A', '2','3','5','5'} ;
     printf("sua mão é:\n");
           
 for(i=0;i<=)
           
         
     return 0;
+}
+
+
+void shuffle(int *array, size_t n)
+{
+    if (n > 1) 
+    {
+        size_t i;
+        for (i = 0; i < n - 1; i++) 
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
+    }
 }

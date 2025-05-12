76f17e2f1494b1d183b2ff5375aeba217d376647
luisfelipe1005 <47942553+luisfelipe1005@users.noreply.github.com>
2025-05-12 11:30:26 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char mao[5]={ 'A', '2','3','5','5'} ;
+    printf("sua mão é:\n");
+          
+for(i=0;i<=)
+          
+        
+    return 0;
+}
